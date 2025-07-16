eb1708797b180e1f6aea36e6e751de22e4446482
tobiama <amaliagillig2508@gmail.com>
2025-07-16 09:54:47 -0300

    Add files via upload

---
+++ final/artistas.js
@@ -0,0 +1,62 @@
+const artistasPop = [
+  {
+    nombre: "Taylor Swift",
+    imagen: "https://i.pinimg.com/1200x/55/dd/45/55dd452eed6881933f939bba84411d0d.jpg",
+    youtube: "https://www.youtube.com/channel/UCqECaJ8Gagnn7YCbPEzWH6g"
+  },
+  {
+    nombre: "Beabadoobee",
+    imagen: "https://i.pinimg.com/736x/f3/86/ac/f386ac86c5d031c16162137024115b0e.jpg",
+    youtube: "https://www.youtube.com/@Beabadoobee"
+  },
+  {
+    nombre: "Charli XCX",
+    imagen: "https://i.pinimg.com/736x/ee/9e/9f/ee9e9fa19ee2ed0dca7dca29e4b718a3.jpg",
+    youtube: "https://www.youtube.com/@officialcharlixcx"
+  },
+  {
+    nombre: "Billie Eilish",
+    imagen: "https://i.pinimg.com/736x/21/65/16/2165162a2950b5b6150b94dde3bf5939.jpg",
+    youtube: "https://www.youtube.com/channel/UCiGm_E4ZwYSHV3bcW1pnSeQ"
+  },
+  {
+    nombre: "Marina And The Diamonds",
+    imagen: "https://i.pinimg.com/736x/96/2d/7a/962d7a90261be76740a1fb424104c735.jpg",
+    youtube: "https://www.youtube.com/@marinadiamandis"
+  },
+  {
+    nombre: "Sabrina Carpenter",
+    imagen: "https://i.pinimg.com/736x/41/3c/7c/413c7c64f0d36225e6945a46d2932177.jpg",
+    youtube: "https://www.youtube.com/channel/UCPKWE1H6xhxwPlqUlKgHb_w"
+  },
+  {
+    nombre: "Clairo",
+    imagen: "https://media.vanityfair.com/photos/66db49d0b2332eb1a8556bdf/1:1/w_2806,h_2806,c_limit/Clairo_Credit_Lucas%20Creighton.jpg",
+    youtube: "https://www.youtube.com/channel/UCa-iCk5rSuWnAb_VT9Hzs3w"
+  },
+  {
+    nombre: "Chappell Roan",
+    imagen: "https://i.pinimg.com/736x/4a/77/ca/4a77ca16231075ce210957ed00a77c7e.jpg",
+    youtube: "https://www.youtube.com/channel/UCTKTRVaWrythRIGNfZYBp2A"
+  },
+  {
+    nombre: "PinkPantheress",
+    imagen: "https://i.pinimg.com/736x/3e/b1/1a/3eb11a66b73616c7f76f3c7d58f96c90.jpg",
+    youtube: "https://www.youtube.com/@ponies721"
+  },
+  {
+    nombre: "Olivia Rodrigo",
+    imagen: "https://i.pinimg.com/1200x/18/c6/08/18c60882db095bfc09d284b4716aa6b7.jpg",
+    youtube: "https://www.youtube.com/channel/UCy3zgWom-5AGypGX_FVTKpg"
+  },
+  {
+    nombre: "Tate McRae",
+    imagen: "https://i.pinimg.com/1200x/83/25/be/8325bea8737096c030fdfaabaa6a5122.jpg",
+    youtube: "https://www.youtube.com/@TateMcRae"
+  },
+  {
+    nombre: "Addison Rae",
+    imagen: "https://i.pinimg.com/736x/f9/4b/d5/f94bd512d5ac0197bcb7e21cd6501c82.jpg",
+    youtube: "https://www.youtube.com/channel/UCsjVdwXJydmlSLVT2zDuwpQ"
+  }
+];
---
+++ final/datosPerfil.js
@@ -0,0 +1,157 @@
+const datosPerfil = {
+  "Clairo": {
+    subtitulo: "Cantautora indie-pop",
+    descripcion: "Clairo se hizo famosa con su sencillo 'Pretty Girl' y es reconocida por su estilo suave y nostálgico.",
+    cumpleaños: "18 de agosto de 1998",
+    edad: "25 años",
+    estadisticas: {
+      "Oyentes mensuales": "19 000 000",
+      "Álbumes publicados": "3",
+      "Top": "#420",
+      "Años de carrera": "6"
+    }
+  },
+
+  "Charli XCX": {
+    subtitulo: "Pop experimental / hyperpop",
+    descripcion: "Charli XCX fusiona electrónica y pop con una estética atrevida, famosa por álbumes como ‘Crash’ y ‘Brat’.",
+    cumpleaños: "2 de agosto de 1992",
+    edad: "32 años",
+    estadisticas: {
+      "Oyentes mensuales": "36 500 000",
+      "Álbumes publicados": "6",
+      "Top": "#112",  
+      "Años de carrera": "15"
+    }
+  },
+
+  "Beabadoobee": {
+    subtitulo: "Bedroom / indie rock",
+    descripcion: "Beabadoobee saltó a la fama con su estilo íntimo y melancólico, y su tercer disco debutó #1 en UK.",
+    cumpleaños: "3 de junio de 2000",
+    edad: "25 años",
+    estadisticas: {
+      "Oyentes mensuales": "23 700 000",
+      "Álbumes publicados": "3",
+      "Top": "#288",
+      "Años de carrera": "6"
+    }
+  },
+
+  "PinkPantheress": {
+    subtitulo: "Pop / UK garage revival",
+    descripcion: "PinkPantheress combina nostalgia 2-step con letras introspectivas en canciones cortas y pegadizas.",
+    cumpleaños: "18 de abril de 2001",  
+    edad: "24 años",
+    estadisticas: {
+      "Oyentes mensuales": "22 720 000" ,
+      "Álbumes publicados": "1",
+      "Top": "#311",
+      "Años de carrera": "4"
+    }
+  },
+
+  "Sabrina Carpenter": {
+    subtitulo: "Pop / dance-pop",
+    descripcion: "Sabrina mezcla pop con matices de R&B; reconocida en EE.UU. por canciones como “Nonsense”.",
+    cumpleaños: "11 de mayo de 1999",
+    edad: "26 años",
+    estadisticas: {
+      "Oyentes mensuales": "70 900 000",  
+      "Álbumes publicados": "6",
+      "Top": "#18",
+      "Años de carrera": "10"
+    }
+  },
+
+  "Taylor Swift": {
+    subtitulo: "Cantautora / pop & country",
+    descripcion: "Taylor es una de las artistas más influyentes de la historia, famosa por su evolución musical y narrativa lírica.",
+    cumpleaños: "13 de diciembre de 1989",
+    edad: "35 años",
+    estadisticas: {
+      "Oyentes mensuales": "81 340 000",  
+      "Álbumes publicados": "10",
+      "Top": "#9",
+      "Años de carrera": "19"
+    }
+  },
+
+  "Olivia Rodrigo": {
+    subtitulo: "Pop rock / indie pop",
+    descripcion: "Olivia irrumpió en 2021 con 'Drivers License' y consolidó su éxito con álbumes introspectivos.",
+    cumpleaños: "20 de febrero de 2003",
+    edad: "22 años",
+    estadisticas: {
+      "Oyentes mensuales": "45 850 000" ,
+      "Álbumes publicados": "2",
+      "Top": "#66",
+      "Años de carrera": "4"
+    }
+  },
+
+  "Addison Rae": {
+    subtitulo: "Pop / social media influencer",
+    descripcion: "Addison saltó a la fama en TikTok y lanzó música pop orientada a su base millennial.",
+    cumpleaños: "6 de octubre de 2000",
+    edad: "24 años",
+    estadisticas: {
+      "Oyentes mensuales": "16 000 000" ,
+      "Álbumes publicados": "1",
+      "Top": "#702",
+      "Años de carrera": "2"
+    }
+  },
+
+  "Chappell Roan": {
+    subtitulo: "Pop teatral / alt-pop",
+    descripcion: "Chappell se distingue por su estilo dramático y ha ganado reconocimiento con su álbum debut y estilo visual.",
+    cumpleaños: "26 de octubre de 1998",
+    edad: "26 años",
+    estadisticas: {
+      "Oyentes mensuales": "40 000 000" ,
+      "Álbumes publicados": "1",
+      "Top": "#94",
+      "Años de carrera": "3"
+    }
+  },
+
+  "Billie Eilish": {
+    subtitulo: "Electropop / dark pop",
+    descripcion: "Billie irrumpió con 'Ocean Eyes' y se destacó globalmente por su voz suave y estética oscura.",
+    cumpleaños: "18 de diciembre de 2001",
+    edad: "23 años",
+    estadisticas: {
+      "Oyentes mensuales": "93 420 000", 
+      "Álbumes publicados": "3",
+      "Top": "#6",
+      "Años de carrera": "7"
+    }
+  },
+
+  "Tate McRae": {
+    subtitulo: "Pop / dance-pop",
+    descripcion: "Tate, surgida de YouTube, combina baile y pop moderno; su éxito 'you broke me first' la catapultó.",
+    cumpleaños: "1 de julio de 2003",
+    edad: "22 años",
+    estadisticas: {
+      "Oyentes mensuales": "56 260 000",  
+      "Álbumes publicados": "2",
+      "Top": "#37",
+      "Años de carrera": "5"
+    }
+  },
+
+  "Marina And The Diamonds": {
+    subtitulo: "Pop indie / art pop",
+    descripcion: "Marina es conocida por su estilo conceptual y voz distintiva, famosa en UK indie pop.",
+    cumpleaños: "10 de octubre de 1985",
+    edad: "39 años",
+    estadisticas: {
+      "Oyentes mensuales": "10 600 000" ,
+      "Álbumes publicados": "5",
+      "Top": "#678",
+      "Años de carrera": "15"
+    }
+  }
+};
